0abe7cb65dd7af9349bf6a075e20136d3901d57c
Terry <168416857+pdahd@users.noreply.github.com>
2024-06-27 16:22:44 +0800

    Update vf_gltransition.c

--- vf_gltransition.c
+++ vf_gltransition.c
@@ -3,6 +3,7 @@
  *
  * @see https://gl-transitions.com/
  */
+
 #include "libavutil/opt.h"
 #include "internal.h"
 #include "framesync.h"
@@ -16,8 +17,9 @@
 # include <EGL/egl.h>
 # include <EGL/eglext.h>
 # include <GLES2/gl2.h>
+# include <GLES2/gl2ext.h>
 #else
-# error "This filter only supports EGL and OpenGL ES2."
+# error "This filter only supports EGL and OpenGL ES 2.0."
 #endif
 
 #include <stdio.h>
@@ -26,6 +28,7 @@
 
 #define FROM (0)
 #define TO   (1)
+
 #define PIXEL_FORMAT (GL_RGB)
 
 static const EGLint configAttribs[] = {
@@ -38,12 +41,6 @@ static const EGLint configAttribs[] = {
     EGL_NONE
 };
 
-static const EGLint pbufferAttribs[] = {
-    EGL_WIDTH, 1,
-    EGL_HEIGHT, 1,
-    EGL_NONE,
-};
-
 static const float position[12] = {
   -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f
 };
@@ -52,12 +49,13 @@ static const GLchar *v_shader_source =
   "attribute vec2 position;\n"
   "varying vec2 _uv;\n"
   "void main(void) {\n"
-  "  gl_Position = vec4(position, 0, 1);\n"
+  "  gl_Position = vec4(position, 0.0, 1.0);\n"
   "  vec2 uv = position * 0.5 + 0.5;\n"
   "  _uv = vec2(uv.x, 1.0 - uv.y);\n"
   "}\n";
 
 static const GLchar *f_shader_template =
+  "precision mediump float;\n" // OpenGL ES 需要精度限定符
   "varying vec2 _uv;\n"
   "uniform sampler2D from;\n"
   "uniform sampler2D to;\n"
@@ -92,12 +90,15 @@ static const GLchar *f_default_transition_source =
 typedef struct {
   const AVClass *class;
   FFFrameSync fs;
+
   // input options
   double duration;
   double offset;
   char *source;
+
   // timestamp of the first frame in the output, in the timebase units
   int64_t first_pts;
+
   // uniforms
   GLuint        from;
   GLuint        to;
@@ -105,13 +106,17 @@ typedef struct {
   GLint         ratio;
   GLint         _fromR;
   GLint         _toR;
+
   // internal state
   GLuint        posBuf;
   GLuint        program;
+#ifdef GL_TRANSITION_USING_EGL
   EGLDisplay eglDpy;
   EGLConfig eglCfg;
   EGLSurface eglSurf;
   EGLContext eglCtx;
+#endif
+
   GLchar *f_shader_source;
 } GLTransitionContext;
 
@@ -120,7 +125,7 @@ typedef struct {
 
 static const AVOption gltransition_options[] = {
   { "duration", "transition duration in seconds", OFFSET(duration), AV_OPT_TYPE_DOUBLE, {.dbl=1.0}, 0, DBL_MAX, FLAGS },
-  { "offset", "delay before startingtransition in seconds", OFFSET(offset), AV_OPT_TYPE_DOUBLE, {.dbl=0.0}, 0, DBL_MAX, FLAGS },
+  { "offset", "delay before starting transition in seconds", OFFSET(offset), AV_OPT_TYPE_DOUBLE, {.dbl=0.0}, 0, DBL_MAX, FLAGS },
   { "source", "path to the gl-transition source file (defaults to basic fade)", OFFSET(source), AV_OPT_TYPE_STRING, {.str = NULL}, CHAR_MIN, CHAR_MAX, FLAGS },
   {NULL}
 };
@@ -129,18 +134,18 @@ FRAMESYNC_DEFINE_CLASS(gltransition, GLTransitionContext, fs);
 
 static GLuint build_shader(AVFilterContext *ctx, const GLchar *shader_source, GLenum type)
 {
-  GLint status;
   GLuint shader = glCreateShader(type);
-  if (!shader || !glIsShader(shader)) {
+  if (!shader) {
     av_log(ctx, AV_LOG_ERROR, "Failed to create shader (type: %d)\n", type);
     return 0;
   }
 
-  glShaderSource(shader, 1, &shader_source, 0);
+  glShaderSource(shader, 1, &shader_source, NULL);
   glCompileShader(shader);
 
+  GLint status;
   glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-  if (status != GL_TRUE) {
+  if (!status) {
     GLint logLength;
     glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
     if (logLength > 0) {
@@ -149,6 +154,7 @@ static GLuint build_shader(AVFilterContext *ctx, const GLchar *shader_source, GL
       av_log(ctx, AV_LOG_ERROR, "Shader compilation failed:\n%s\n", log);
       free(log);
     }
+    glDeleteShader(shader);
     return 0;
   }
 
@@ -195,7 +201,7 @@ static int build_program(AVFilterContext *ctx)
 
   transition_source = source ? source : f_default_transition_source;
   len = strlen(f_shader_template) + strlen(transition_source);
-  c->f_shader_source = av_calloc(len, sizeof(*c->f_shader_source));
+  c->f_shader_source = av_calloc(len + 1, sizeof(*c->f_shader_source));
   if (!c->f_shader_source) {
     free(source);
     return AVERROR(ENOMEM);
@@ -223,7 +229,7 @@ static int build_program(AVFilterContext *ctx)
   glLinkProgram(c->program);
 
   glGetProgramiv(c->program, GL_LINK_STATUS, &status);
-  if (status != GL_TRUE) {
+  if (!status) {
     GLint logLength;
     glGetProgramiv(c->program, GL_INFO_LOG_LENGTH, &logLength);
     if (logLength > 0) {
@@ -232,6 +238,7 @@ static int build_program(AVFilterContext *ctx)
       av_log(ctx, AV_LOG_ERROR, "Program linking failed:\n%s\n", log);
       free(log);
     }
+    glDeleteProgram(c->program);
     return -1;
   }
 
@@ -289,7 +296,7 @@ static int init_gl(AVFilterContext *ctx)
     EGL_CONTEXT_CLIENT_VERSION, 2,
     EGL_NONE
   };
-    
+
   av_log(ctx, AV_LOG_DEBUG, "Initializing GL...\n");
 
   c->eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
@@ -308,7 +315,16 @@ static int init_gl(AVFilterContext *ctx)
     return -1;
   }
 
-  c->eglSurf = eglCreatePbufferSurface(c->eglDpy, c->eglCfg, pbufferAttribs);
+  EGLint pbufferAttribs[] = {
+      EGL_WIDTH,
+      1,
+      EGL_HEIGHT,
+      1,
+      EGL_NONE,
+  };
+
+  c->eglSurf = eglCreatePbufferSurface(c->eglDpy, c->eglCfg,
+                                       pbufferAttribs);
   if (c->eglSurf == EGL_NO_SURFACE) {
     av_log(ctx, AV_LOG_ERROR, "Failed to create EGL surface\n");
     return -1;
@@ -360,117 +376,121 @@ static int config_input(AVFilterLink *inlink)
   return 0;
 }
 
-static int filter_frame(FFFrameSync *fs, AVFrame **out, int index)
+static AVFrame *apply_transition(FFFrameSync *fs,
+                                 AVFilterContext *ctx,
+                                 AVFrame *fromFrame,
+                                 const AVFrame *toFrame)
 {
-  AVFilterContext *ctx = fs->parent;
   GLTransitionContext *c = ctx->priv;
-  AVFrame *in;
-  int ret = ff_framesync_get_frame(fs, index, &in, 0);
+  AVFilterLink *fromLink = ctx->inputs[FROM];
+  AVFilterLink *toLink = ctx->inputs[TO];
+  AVFilterLink *outLink = ctx->outputs[0];
+  AVFrame *outFrame;
 
-  av_log(ctx, AV_LOG_DEBUG, "Filtering frame...\n");
-
-  if (ret < 0) {
-    av_log(ctx, AV_LOG_ERROR, "Failed to get frame: %s\n", av_err2str(ret));
-    return ret;
+  outFrame = ff_get_video_buffer(outLink, outLink->w, outLink->h);
+  if (!outFrame) {
+    return NULL;
   }
+
+  av_frame_copy_props(outFrame, fromFrame);
+
+#ifdef GL_TRANSITION_USING_EGL
+  eglMakeCurrent(c->eglDpy, c->eglSurf, c->eglSurf, c->eglCtx);
+#endif
 
   glUseProgram(c->program);
 
-  glUniform1f(c->progress, (float)(*out)->pts / (float)(c->duration * AV_TIME_BASE));
-  glUniform1f(c->ratio, (float)(*out)->width / (float)(*out)->height);
-  glUniform1f(c->_fromR, 1.0f);
-  glUniform1f(c->_toR, 1.0f);
+  const float ts = ((fs->pts - c->first_pts) / (float)fs->time_base.den) - c->offset;
+  const float progress = FFMAX(0.0f, FFMIN(1.0f, ts / c->duration));
+  glUniform1f(c->progress, progress);
+
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, c->from);
-  glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, in->width, in->height, PIXEL_FORMAT, GL_UNSIGNED_BYTE, in->data[0]);
+  glPixelStorei(GL_UNPACK_ROW_LENGTH, fromFrame->linesize[0] / 3);
+  glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, fromLink->w, fromLink->h, GL_RGB, GL_UNSIGNED_BYTE, fromFrame->data[0]);
 
   glActiveTexture(GL_TEXTURE0 + 1);
   glBindTexture(GL_TEXTURE_2D, c->to);
-  glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, in->width, in->height, PIXEL_FORMAT, GL_UNSIGNED_BYTE, in->data[0]);
+  glPixelStorei(GL_UNPACK_ROW_LENGTH, toFrame->linesize[0] / 3);
+  glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, toLink->w, toLink->h, GL_RGB, GL_UNSIGNED_BYTE, toFrame->data[0]);
 
   glDrawArrays(GL_TRIANGLES, 0, 6);
 
-  eglSwapBuffers(c->eglDpy, c->eglSurf);
+  // 读取渲染结果到 CPU 内存
+  glPixelStorei(GL_PACK_ALIGNMENT, 1);
+  glPixelStorei(GL_PACK_ROW_LENGTH, outFrame->linesize[0] / 3);
+  glReadPixels(0, 0, outLink->w, outLink->h, GL_RGB, GL_UNSIGNED_BYTE, outFrame->data[0]);
 
-  av_log(ctx, AV_LOG_DEBUG, "Frame filtered successfully\n");
+  glPixelStorei(GL_PACK_ROW_LENGTH, 0);
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+  glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
 
-  return ff_filter_frame(ctx->outputs[0], *out);
+  av_frame_free(&fromFrame);
+
+  return outFrame;
 }
 
-static int filter_frame_event(FFFrameSync *fs)
+static int blend_frame(FFFrameSync *fs)
 {
   AVFilterContext *ctx = fs->parent;
-  AVFrame *out;
-  int ret = filter_frame(fs, &out, 0);
+  GLTransitionContext *c = ctx->priv;
 
-  av_log(ctx, AV_LOG_DEBUG, "Filtering frame event...\n");
+  AVFrame *fromFrame, *toFrame, *outFrame;
+  int ret;
 
+  ret = ff_framesync_dualinput_get(fs, &fromFrame, &toFrame);
   if (ret < 0) {
-    av_log(ctx, AV_LOG_ERROR, "Failed to filter frame: %s\n", av_err2str(ret));
     return ret;
   }
 
-  av_log(ctx, AV_LOG_DEBUG, "Frame event filtered successfully\n");
+  if (c->first_pts == AV_NOPTS_VALUE && fromFrame && fromFrame->pts != AV_NOPTS_VALUE) {
+    c->first_pts = fromFrame->pts;
+  }
 
-  return ff_filter_frame(ctx->outputs[0], out);
+  if (!toFrame) {
+    return ff_filter_frame(ctx->outputs[0], fromFrame);
+  }
+
+  outFrame = apply_transition(fs, ctx, fromFrame, toFrame);
+  if (!outFrame) {
+    return AVERROR(ENOMEM);
+  }
+
+  return ff_filter_frame(ctx->outputs[0], outFrame);
 }
 
 static av_cold int init(AVFilterContext *ctx)
 {
   GLTransitionContext *c = ctx->priv;
-  int ret;
+  c->fs.on_event = blend_frame;
+  c->first_pts = AV_NOPTS_VALUE;
 
-  av_log(ctx, AV_LOG_DEBUG, "Initializing filter...\n");
-
-  ret = ff_framesync_init(&c->fs, ctx, 2);
-  if (ret < 0) {
-    av_log(ctx, AV_LOG_ERROR, "ff_framesync_init failed: %s\n", av_err2str(ret));
-    return ret;
-  }
-  av_log(ctx, AV_LOG_DEBUG, "framesync initialized, nb_outputs: %d\n", ctx->nb_outputs); // 使用 ctx->nb_outputs
-
-  c->fs.on_event = filter_frame_event;
-
-  av_log(ctx, AV_LOG_DEBUG, "Filter initialized successfully\n");
   return 0;
 }
 
-static av_cold void uninit(AVFilterContext *ctx)
-{
+static av_cold void uninit(AVFilterContext *ctx) {
   GLTransitionContext *c = ctx->priv;
+  ff_framesync_uninit(&c->fs);
 
-  av_log(ctx, AV_LOG_DEBUG, "Uninitializing filter...\n");
+#ifdef GL_TRANSITION_USING_EGL
+  if (c->eglDpy) {
+    glDeleteTextures(1, &c->from);
+    glDeleteTextures(1, &c->to);
+    glDeleteBuffers(1, &c->posBuf);
+    glDeleteProgram(c->program);
+    eglMakeCurrent(c->eglDpy, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+    eglDestroyContext(c->eglDpy, c->eglCtx);
+    eglDestroySurface(c->eglDpy, c->eglSurf);
+    eglTerminate(c->eglDpy);
+  }
+#endif
 
-  eglDestroyContext(c->eglDpy, c->eglCtx);
-  eglDestroySurface(c->eglDpy, c->eglSurf);
-  eglTerminate(c->eglDpy);
-  av_freep(&c->f_shader_source);
-
-  av_log(ctx, AV_LOG_DEBUG, "Filter uninitialized\n");
+  if (c->f_shader_source) {
+    av_freep(&c->f_shader_source);
+  }
 }
-
-static const AVFilterPad gltransition_inputs[] = {
-  {
-    .name         = "from",
-    .type         = AVMEDIA_TYPE_VIDEO,
-    .config_props = config_input,
-  },
-  {
-    .name         = "to",
-    .type         = AVMEDIA_TYPE_VIDEO,
-    .config_props = config_input,
-  },
-  { NULL }
-};
-
-static const AVFilterPad gltransition_outputs[] = {
-  {
-    .name = "default",
-    .type = AVMEDIA_TYPE_VIDEO,
-  },
-  { NULL }
-};
 
 static int query_formats(AVFilterContext *ctx)
 {
@@ -478,13 +498,14 @@ static int query_formats(AVFilterContext *ctx)
     AV_PIX_FMT_RGB24,
     AV_PIX_FMT_NONE
   };
+
   return ff_set_common_formats(ctx, ff_make_format_list(formats));
 }
 
 static int activate(AVFilterContext *ctx)
 {
   GLTransitionContext *c = ctx->priv;
-  int ret; // 将 ret 变量的声明移到代码块开头
+  int ret;
 
   av_log(ctx, AV_LOG_DEBUG, "Activating filter...\n");
   ret = ff_framesync_activate(&c->fs);
@@ -496,16 +517,73 @@ static int activate(AVFilterContext *ctx)
   return ret;
 }
 
+static int config_output(AVFilterLink *outLink)
+{
+  AVFilterContext *ctx = outLink->src;
+  GLTransitionContext *c = ctx->priv;
+  AVFilterLink *fromLink = ctx->inputs[FROM];
+  AVFilterLink *toLink = ctx->inputs[TO];
+  int ret;
+
+  if (fromLink->format != toLink->format) {
+    av_log(ctx, AV_LOG_ERROR, "inputs must be of same pixel format\n");
+    return AVERROR(EINVAL);
+  }
+
+  if (fromLink->w != toLink->w || fromLink->h != toLink->h) {
+    av_log(ctx, AV_LOG_ERROR, "First input link %s parameters "
+           "(size %dx%d) do not match the corresponding "
+           "second input link %s parameters (size %dx%d)\n",
+           ctx->input_pads[FROM].name, fromLink->w, fromLink->h,
+           ctx->input_pads[TO].name, toLink->w, toLink->h);
+    return AVERROR(EINVAL);
+  }
+
+  outLink->w = fromLink->w;
+  outLink->h = fromLink->h;
+  outLink->frame_rate = fromLink->frame_rate;
+
+  if ((ret = ff_framesync_init_dualinput(&c->fs, ctx)) < 0) {
+    return ret;
+  }
+
+  return ff_framesync_configure(&c->fs);
+}
+
+static const AVFilterPad gltransition_inputs[] = {
+  {
+    .name = "from",
+    .type = AVMEDIA_TYPE_VIDEO,
+    .config_props = config_input,
+  },
+  {
+    .name = "to",
+    .type = AVMEDIA_TYPE_VIDEO,
+    .config_props = config_input,
+  },
+  {NULL}
+};
+
+static const AVFilterPad gltransition_outputs[] = {
+  {
+    .name = "default",
+    .type = AVMEDIA_TYPE_VIDEO,
+    .config_props = config_output,
+  },
+  {NULL}
+};
+
 AVFilter ff_vf_gltransition = {
   .name          = "gltransition",
   .description   = NULL_IF_CONFIG_SMALL("Apply GLSL transitions between video streams."),
   .priv_size     = sizeof(GLTransitionContext),
+  .preinit       = gltransition_framesync_preinit,
   .init          = init,
   .uninit        = uninit,
-  .formats.query_func    = query_formats,
+  .query_formats = query_formats,
   .activate      = activate,
   .inputs        = gltransition_inputs,
   .outputs       = gltransition_outputs,
   .priv_class    = &gltransition_class,
-  .flags         = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC,
+  .flags         = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC
 };
